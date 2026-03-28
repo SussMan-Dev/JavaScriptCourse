f3034ae84e4747aee59b04aa6336258d90ee0f6b
SussMan-Dev <duchuyendfield@gmail.com>
2026-03-28 20:45:44 +0700

    update console guide

---
+++ console/console.js
@@ -0,0 +1,15 @@
+//========================================CONSOLE========================================
+let a = 'javaScript'
+let b = 'Course'
+console.log(a + b);
+console.warn("This is warning status!!")
+console.error("Error Error, must be the wind")
+console.info("This is neccessary infomation dude")
+//========================================TEMPLATE STRING========================================
+// We knew this from Datatype Part of this cource, this is we called "String", it is a datatype using for store "text"
+// when you log the text to the string, you usually do this
+let string1 = 'javaScript'
+let string2 = 'Course'
+console.log(string1 + string2);
+// but with this template string, you can write like this
+console.log(`with template string, you can log the text like this: ${string1} ${string2}`);
